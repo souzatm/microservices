bb9a8a61be016fb29980f4396583bb5fa3471e53
souzatm <tsouzatm@outlook.com>
2023-02-07 20:53:11 -0300

    Configurando Clients e configuração de BD e segurança

--- sec10-consumindo-microservice-web-net5/GeekShooping/GeekShopping.IdentityServer/Startup.cs
+++ sec10-consumindo-microservice-web-net5/GeekShooping/GeekShopping.IdentityServer/Startup.cs
@@ -1,5 +1,10 @@
+using GeekShopping.IdentityServer.Configuration;
+using GeekShopping.IdentityServer.Model;
+using GeekShopping.IdentityServer.Model.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -22,6 +27,31 @@ namespace GeekShopping.IdentityServer
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connection = Configuration["MySQlConnection:MySQlConnectionString"];
+
+            services.AddDbContext<MySQLContext>(options => options.
+                UseMySql(connection,
+                        new MySqlServerVersion(
+                            new Version(8, 0, 5))));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<MySQLContext>()
+                .AddDefaultTokenProviders();
+
+            var builder = services.AddIdentityServer(options =>
+            {
+                options.Events.RaiseErrorEvents = true;
+                options.Events.RaiseInformationEvents = true;
+                options.Events.RaiseFailureEvents = true;
+                options.Events.RaiseSuccessEvents = true;
+                options.EmitStaticAudienceClaim = true;
+            }).AddInMemoryIdentityResources(
+                IdentityConfiguration.IdentityResources)
+            .AddInMemoryClients(IdentityConfiguration.Clients)
+            .AddAspNetIdentity<ApplicationUser>();
+
+            builder.AddDeveloperSigningCredential();
+
             services.AddControllersWithViews();
         }
 
@@ -39,6 +69,8 @@ namespace GeekShopping.IdentityServer
             app.UseStaticFiles();
 
             app.UseRouting();
+
+            app.UseIdentityServer();
 
             app.UseAuthorization();
 
